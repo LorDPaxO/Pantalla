fc0854bd83c2be06d389c2b0182d87778a9eeca5
LorDPaxO <luzbinbautista@hotmail.com>
2018-01-28 17:51:36 -0500

    //
    Subido desde el back up del pc

--- Plantilla_Programacion_Arduino/Plantilla_Base/Plantilla_Base.ino
+++ Plantilla_Programacion_Arduino/Plantilla_Base/Plantilla_Base.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #include <Nextion.h>
 
-SoftwareSerial nextion(2, 3);// Nextion TX to pin 2 and RX to pin 3 of Arduino 
+SoftwareSerial nextion(7, 6);// Nextion TX to pin 2 and RX to pin 3 of Arduino 
 Nextion myNextion(nextion, 9600); //create a Nextion object named myNextion using the nextion serial port @ 9600bps
 
 
@@ -9,7 +9,7 @@ void setup() {
   Serial.begin(9600);
   myNextion.init(); // send the initialization commands for Page 0
   delay(1500);
-  myNextion.sendCommand("page 1");
+  myNextion.sendCommand("page Cortando");
 
 }
 
